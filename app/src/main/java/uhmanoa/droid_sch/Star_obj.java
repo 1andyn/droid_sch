808df8abdf33c5019be87337946e2970ce6b06ed
andyng <andyng@hawaii.edu>
2015-01-27 20:12:59 -1000

    refactored star_obj class

--- app/src/main/java/uhmanoa/droid_sch/Star_obj.java
+++ app/src/main/java/uhmanoa/droid_sch/Star_obj.java
@@ -3,13 +3,13 @@ package uhmanoa.droid_sch;
 /**
  * Created by supah_000 on 1/27/2015.
  */
-public class star_obj {
+public class Star_obj {
     private String course_name;
     private int CRN;
     private int ID; // Incase user for some reason stars both a course and class
 
     //Constructor
-    public star_obj(String pCrsname, int pCRN, int pID) {
+    public Star_obj(String pCrsname, int pCRN, int pID) {
         pID= ID;
         CRN = pCRN;
         course_name = pCrsname;
