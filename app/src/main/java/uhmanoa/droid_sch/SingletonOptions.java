e859dd11c873fc8c3f3e578ec9da49893bb307a4
andyng <andyng@hawaii.edu>
2015-04-08 01:06:02 -1000

    added singleton option class for passing builder option data to results activity

---
+++ app/src/main/java/uhmanoa/droid_sch/SingletonOptions.java
@@ -0,0 +1,63 @@
+package uhmanoa.droid_sch;
+
+
+public class SingletonOptions {
+    private static SingletonOptions instance = null;
+	private boolean en_StartTime = false;
+    private boolean en_EndTime = false;
+    private int startTime = 0;
+    private int endTime = 0;
+    private int mincrs = -1;
+
+	private SingletonOptions(){
+        //do nothing
+	}
+
+    public static SingletonOptions getInstance() {
+        if(instance == null) {
+            instance = new SingletonOptions();
+        }
+        return instance;
+    }
+
+    public void setEn_StartTime(int time, boolean en) {
+        en_StartTime = en;
+        startTime = time;
+    }
+
+    public void setEn_EndTime(int time, boolean en) {
+        en_EndTime = en;
+        endTime = time;
+    }
+
+    public boolean getEnStart()
+    {
+        return en_StartTime;
+    }
+
+    public boolean getEnEnd() {
+        return en_EndTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public int getMinCourse() {
+        return mincrs;
+    }
+
+    public void setMinCrs(int crs) {
+        mincrs = crs;
+    }
+
+    public boolean defaultCrs() {
+        return mincrs == -1;
+    }
+
+
+}
