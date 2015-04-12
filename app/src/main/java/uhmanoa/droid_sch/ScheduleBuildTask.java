cf302ee6c7950b56bc5f60b48a182a377fb7f299
andyng <andyng@hawaii.edu>
2015-04-11 20:04:44 -1000

    moved redudancy checks to the asynchronous task

--- app/src/main/java/uhmanoa/droid_sch/ScheduleBuildTask.java
+++ app/src/main/java/uhmanoa/droid_sch/ScheduleBuildTask.java
@@ -16,6 +16,7 @@ public class ScheduleBuildTask extends AsyncTask<Void, Void, Integer> {
     private SQL_DataSource data;
     private int year;
     private int semester;
+    private int min;
 
     @Override
     protected void onPreExecute() {
@@ -28,19 +29,22 @@ public class ScheduleBuildTask extends AsyncTask<Void, Void, Integer> {
 
     //constructor, doesnt need to do anything for now
     public ScheduleBuildTask(Context c, SQL_DataSource ds, OnBuildTaskComplete listener,
-                             int sem, int yr, ArrayList<Star_obj> so) {
+                             int sem, int yr, ArrayList<Star_obj> so, int mini) {
         this.listener = listener;
         data = ds;
         year = yr;
         semester = sem;
         app_context = c;
         inputs = so;
+        min = mini;
     }
 
     @Override
     protected Integer doInBackground(Void... params) {
-        ClassScheduler cs = new ClassScheduler(semester, year, data);
-        results = cs.getPossibleSchedules(inputs);
+        ArrayList<Star_obj> filtered_list = filterCourses(inputs);
+        ClassScheduler cs = new ClassScheduler(semester, year, data, min,
+                getCrnExclusive(filtered_list));
+        results = cs.getPossibleSchedules(filtered_list);
         return 1;
     }
 
@@ -53,6 +57,53 @@ public class ScheduleBuildTask extends AsyncTask<Void, Void, Integer> {
         pdialog.dismiss();
         listener.onBuildTaskComplete();
     }
+
+    private ArrayList<Star_obj> getCrnExclusive(ArrayList<Star_obj> ex) {
+        ArrayList<Star_obj> so = new ArrayList<>();
+        for(Star_obj s : ex) {
+            if(s.getCRN() != -1) {
+                so.add(s);
+            }
+        }
+        return so;
+    }
+
+    private ArrayList<Star_obj> filterCourses(ArrayList<Star_obj> list) {
+        // ----------------------- REDUNDANCY CHECK ---------------------------------
+        ArrayList<String> courses = new ArrayList<>(); //Contains List of "Named" of Courses
+        //Obtain List of Courses listed by Name
+        for (Star_obj s : list) {
+            String crs = s.getCourse();
+            if (!courses.contains(crs) && s.getCRN() == -1) {
+                //The list doesn't already contain the course and
+                courses.add(crs);
+            }
+        }
+
+        //Create new Star_Obj list containg non-reduant Star_Objs
+        ArrayList<Star_obj> course_list = new ArrayList<>();
+        //Iterate through current list
+        for (Star_obj s : list) {
+            boolean delete_current = false;
+            for (String c : courses) {
+                if (s.getCourse().equals(c) && s.getCRN() != -1) {
+                    delete_current = true; //this star_obj is repeated
+                    break;
+                }
+            }
+            if (!delete_current) {
+                course_list.add(s);
+            }
+
+        }
+        // ----------------------- REDUNDANCY CHECK ---------------------------------
+        return course_list;
+    }
+
+
+
+
+
 
 }
 
