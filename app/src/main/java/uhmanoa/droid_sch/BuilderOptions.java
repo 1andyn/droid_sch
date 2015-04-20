0605d1c8e5a5196eb8aae635e10adaa87f1f332b
andyng <andyng@hawaii.edu>
2015-04-19 23:45:51 -1000

    added preference writer helper functions

--- app/src/main/java/uhmanoa/droid_sch/BuilderOptions.java
+++ app/src/main/java/uhmanoa/droid_sch/BuilderOptions.java
@@ -23,6 +23,12 @@ public class BuilderOptions{
     private final String intEnd = "PREF_END_INT";
 
     private final String booleanTimeOff = "PREF_TIME_OFF";
+    private final String stringDayTimeOff1 = "PREF_TIMOFF_DAY1";
+    private final String stringDayTimeOff2 = "PREF_TIMOFF_DAY2";
+    private final String intTOStart1 = "PREF_START_INT_TO1";
+    private final String intTOStart2 = "PREF_START_INT_TO2";
+    private final String intTOEnd1 = "PREF_END_INT_END1";
+    private final String intTOEnd2 = "PREF_END_INT_END2";
 
 
     public BuilderOptions (Context ctx) {
@@ -36,6 +42,62 @@ public class BuilderOptions{
         return x;
     }
 
+    public void setTimeOffStart1(int x) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(intTOStart1, x);
+        editor.commit();
+    }
+
+    public void setTimeOffStart2(int x) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(intTOStart2,x);
+        editor.commit();
+    }
+
+    public void setTimeOffEnd1(int x) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(intTOEnd1, x);
+        editor.commit();
+    }
+
+    public void setTimeOffEnd2(int x) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(intTOEnd2, x);
+        editor.commit();
+    }
+
+    public int getTimeOffStart1() {
+        int x = 0; //default = 0;
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        x = settings.getInt(intTOStart1, -1);
+        return x;
+    }
+
+    public int getTimeOffStart2() {
+        int x = 0; //default = 0;
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        x = settings.getInt(intTOStart2, -1);
+        return x;
+    }
+
+    public int getTimeOffEnd1() {
+        int x = 0; //default = 0;
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        x = settings.getInt(intTOEnd1, -1);
+        return x;
+    }
+
+    public int getTimeOffEnd2() {
+        int x = 0; //default = 0;
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        x = settings.getInt(intTOEnd2, -1);
+        return x;
+    }
+
     public void setSelectedOption(int x) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
         SharedPreferences.Editor editor = settings.edit();
@@ -43,6 +105,16 @@ public class BuilderOptions{
         editor.commit();
     }
 
+    public void setTimeOffBoolean(boolean b) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(booleanTimeOff, b);
+        editor.commit();
+    }
+    public boolean getTimeOffBoolean() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        return settings.getBoolean(booleanTimeOff, false);
+    }
 
     public void setEarliestStartBoolean(boolean b) {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
@@ -86,6 +158,20 @@ public class BuilderOptions{
         editor.commit();
     }
 
+    public void setDayTimeOffString1(String s) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(stringDayTimeOff1, s);
+        editor.commit();
+    }
+
+    public void setDayTimeOffString2(String s) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(stringDayTimeOff2, s);
+        editor.commit();
+    }
+
     public boolean getDaysOffBoolean() {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
         return settings.getBoolean(booleanDaysOff, false);
@@ -100,6 +186,28 @@ public class BuilderOptions{
         }
         return day_char;
     }
+
+    public ArrayList<Character> getDaysTOArray1() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        String days = settings.getString(stringDayTimeOff1 ,"");
+        ArrayList<Character> day_char = new ArrayList<>();
+        for(int x = 0; x< days.length(); x++) {
+            day_char.add(days.charAt(x));
+        }
+        return day_char;
+    }
+
+
+    public ArrayList<Character> getDaysTOArray2() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
+        String days = settings.getString(stringDayTimeOff2 ,"");
+        ArrayList<Character> day_char = new ArrayList<>();
+        for(int x = 0; x< days.length(); x++) {
+            day_char.add(days.charAt(x));
+        }
+        return day_char;
+    }
+
 
     public boolean getBooleanLatestEnd() {
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(c);
